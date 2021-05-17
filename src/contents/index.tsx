0690d5322c3e560f948cd4d527924d1b8479f3cc
Fabian Cook <hello@fabiancook.dev>
2021-05-18 00:50:02 +1200

    Template posts

--- src/contents/index.tsx
+++ src/contents/index.tsx
@@ -1,6 +1,7 @@
 import { h } from "../h";
 import { Intro } from "./intro";
 import * as Posts from "./blog";
+import { Template } from '../template';
 
 const posts = Object.keys(Posts)
   .filter((key: string): key is keyof typeof Posts => Object.prototype.hasOwnProperty.call(Posts, key))
@@ -9,6 +10,8 @@ const posts = Object.keys(Posts)
 export const SiteContents = (
   <main>
     {Intro}
-    {posts}
+    <Template id="posts">
+      {posts}
+    </Template>
   </main>
 );
